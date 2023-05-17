e4eca63d23f743aca603a9be0c1e7bed4d3c449c
josivcoe <josivcoe@gmail.com>
2023-05-17 13:48:21 -0500

    reflect

--- platzi.js
+++ platzi.js
@@ -6,11 +6,14 @@ class PlatziReactive {
         //DESTINO
         this.$data = new Proxy(this.origen, {
             get(target, name){
-                if (name in target) {
-                    return target[name];
+                if (Reflect.has(target, name)) {
+                    return Reflect.get(target, name);
                 }
                 console.warn("La propiedad", name, "no existe");
                 return "";
+            },
+            set(target, name, value){
+                Reflect.set(target, name, value);
             }
         });
     }
@@ -26,14 +29,25 @@ class PlatziReactive {
         document.querySelectorAll("*[p-text]").forEach(el => {
             this.pText(el, this.$data, el.getAttribute("p-text"));
         });
+
+        document.querySelectorAll("*[p-model]").forEach(el => {
+            const name = el.getAttribute("p-model");
+            this.pModel(el, this.$data, name);
+
+            el.addEventListener("input", () => {
+                Reflect.set(this.$data, name, el.value);
+            });
+        });
     }
     pText( el, target, name) {
-        el.innerText = target[name];
+        el.innerText = Reflect.get(target, name);
     }
-    pModel () {}
+    pModel (el, target, name) {
+        el.value = Reflect.get(target, name);
+    }
 }
 var Platzi  = {
     createApp(options) {
-        return new PlatziReactive (options)
+        return new PlatziReactive (options);
     }
 };
